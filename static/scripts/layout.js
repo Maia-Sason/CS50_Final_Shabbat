f7bc76af63859dc880b27db1c39242eaca56d945
Maia Sason <mayasasson2000@gmail.com>
2021-01-07 14:19:53 -0500

    made a very sexy mobile nav bar :))))

--- static/scripts/layout.js
+++ static/scripts/layout.js
@@ -2,3 +2,16 @@
 //     $('.item').removeClass('active')
 //     $().addClass('active')
 // })
+
+$(document).ready(function () {
+    $('.hamburger').on('click', function() {
+        $('.hamburger').css({'transform': 'Rotate(90deg)'}).fadeOut();
+        $('.ham-close').fadeIn().css({'transform': 'Rotate(-90deg)'});
+        $('.nav-mobile').animate({height: "300vh"})
+    })
+    $('.ham-close').on('click', function() {
+        $('.ham-close').css({'transform': 'Rotate(90deg)'}).fadeOut();
+        $('.hamburger').fadeIn().css({'transform': 'Rotate(180deg)'});
+        $('.nav-mobile').animate({height: "0%"})
+    })
+})
