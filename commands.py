d67b35bf4d579184b9e3c263575f8a4ecfa525d0
Maia Sason <mayasasson2000@gmail.com>
2021-01-07 18:10:01 -0500

    trying to initialize postgress with command.py

---
+++ commands.py
@@ -0,0 +1,11 @@
+# url: https://www.youtube.com/watch?v=FKy21FnjKS0
+# create tables command
+import click
+from flask.cli import with_appcontext
+
+from .application import db, User, Bless, Room, Room_bless
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
