a495cbb31bca1f23be56652d35adb56bc4e1ad74
Maia Sason <mayasasson2000@gmail.com>
2021-01-07 19:55:37 -0500

    fixing nav mobile

--- static/scripts/layout.js
+++ static/scripts/layout.js
@@ -7,11 +7,13 @@ $(document).ready(function () {
     $('.hamburger').on('click', function() {
         $('.hamburger').css({'transform': 'Rotate(90deg)'}).fadeOut();
         $('.ham-close').fadeIn().css({'transform': 'Rotate(-90deg)'});
-        $('.nav-mobile').animate({height: "300vh"})
+        $('.nav-mobile').show();
+        $('.nav-mobile').animate({height: "300vh"});
     })
     $('.ham-close').on('click', function() {
         $('.ham-close').css({'transform': 'Rotate(90deg)'}).fadeOut();
         $('.hamburger').fadeIn().css({'transform': 'Rotate(180deg)'});
         $('.nav-mobile').animate({height: "0%"})
+        $('.nav-mobile').hide();
     })
 })
