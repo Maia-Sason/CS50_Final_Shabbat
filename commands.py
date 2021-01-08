8391a8a316265a58306d955a2fe3190036153986
Maia Sason <mayasasson2000@gmail.com>
2021-01-07 19:15:56 -0500

    trying

--- commands.py
+++ commands.py
@@ -3,9 +3,6 @@
 import click
 from flask.cli import with_appcontext
 
-from .application import db, User, Bless, Room, Room_bless
+from application import db, User, Bless, Room, Room_bless
 
-@click.command(name='create_tables')
-@with_appcontext
-def create_tables():
-    db.create_all()
+db.create_all()
